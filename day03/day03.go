d9101b372e6af9e52c29b4112fb3908a582f9529
Charlie Hothersall-Thomas <me@charlie.ht>
2022-12-03 08:25:49 +0000

    Rewrite day 3 part one having seen part two

--- day03/day03.go
+++ day03/day03.go
@@ -1,35 +1,47 @@
 package day03
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Part1(in []string) (int, error) {
 	var totalPriority int
+
 	for _, l := range in {
-		p, err := overlapPriority(l)
+		m := len(l) / 2
+		l, r := l[:m], l[m:]
+
+		p, err := overlapPriority(l, r)
 		if err != nil {
 			return 0, err
 		}
 		totalPriority += p
 	}
+
 	return totalPriority, nil
 }
 
-func overlapPriority(backpack string) (int, error) {
-	m := len(backpack) / 2
-	l, r := backpack[:m], backpack[m:]
-
-	seen := make(map[rune]struct{})
-	for _, x := range l {
-		seen[x] = struct{}{}
-	}
-
-	for _, x := range r {
-		if _, ok := seen[x]; ok {
-			return priority(x)
+func overlapPriority(backpacks ...string) (int, error) {
+	var keep map[rune]struct{}
+	for i, b := range backpacks {
+		keepNext := make(map[rune]struct{})
+		for _, x := range b {
+			if _, ok := keep[x]; i == 0 || ok {
+				keepNext[x] = struct{}{}
+			}
 		}
+		keep = keepNext
 	}
 
-	return 0, fmt.Errorf("day03: no overlap in backpack: %q", backpack)
+	if len(keep) != 1 {
+		return 0, fmt.Errorf("day03: got overlap of %d items, wanted 1", len(keep))
+	}
+
+	for k := range keep {
+		return priority(k)
+	}
+
+	panic("not reached")
 }
 
 func priority(x rune) (int, error) {
